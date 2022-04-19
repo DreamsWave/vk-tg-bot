a34820229b84dba11d3d8097f761a24497cf68c3
idreamzzer <dreamswave.dev@gmail.com>
2022-04-19 20:35:18 +0300

    added tests for chunkString util

--- packages/mocks/src/index.ts
+++ packages/mocks/src/index.ts
@@ -2,6 +2,7 @@ import * as postSimple from './http/post/post-simple.json'
 import * as messageNew from './http/post/vk-events/message-new.json'
 import * as wallPostNew from './http/post/vk-events/wall-post-new.json'
 import * as messages from './ymq/messages.json'
+import { text } from './text'
 
 const http = {
     post: {
@@ -17,4 +18,4 @@ const ycEvents = {
     messages
 }
 
-export { vkEvents, http, ycEvents }
+export { vkEvents, http, ycEvents, text }
