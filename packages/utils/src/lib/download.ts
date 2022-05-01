4608ff046e7ca6c90ca7d65df946ee7264cc5e53
idreamzzer <idreamzzer@gmail.com>
2022-05-01 20:12:11 +0300

    error handling

--- packages/utils/src/lib/download.ts
+++ packages/utils/src/lib/download.ts
@@ -5,7 +5,6 @@ import { FileInfo } from '@yc-bot/types';
 import path from 'path';
 import { extension } from 'mime-types';
 import ytdl from 'ytdl-core';
-import { logger } from '@yc-bot/shared';
 import pathToFfmpeg from 'ffmpeg-static';
 import child_process from 'child_process';
 import { promisify } from 'util';
@@ -17,70 +16,73 @@ export const downloadFile = async (fileUrl: string, saveTo: string, filename: st
 		const request = https.get(fileUrl, async (resp) => {
 			if (resp.headers.location && resp.statusCode === 302) {
 				resolve(await downloadFile(resp.headers.location, saveTo, String(filename)));
-				return;
 			}
-			const size = Math.ceil(parseInt(resp.headers['content-length'], 10) / 1000); // kb
-			const mime = resp.headers['content-type'];
-			const ext = extension(mime);
-			const filePath = path.join(saveTo, `${filename}.${ext}`);
+			if (resp.statusCode === 200) {
+				const size = Math.ceil(parseInt(resp.headers['content-length'], 10) / 1000); // kb
+				const mime = resp.headers['content-type'];
+				const ext = extension(mime);
+				const filePath = path.join(saveTo, `${filename}.${ext}`);
 
-			if (size > 50000) reject('File is bigger than 50mb');
+				if (size > 50000) reject('File is bigger than 50mb');
 
-			const fileStream = fs.createWriteStream(filePath);
-			resp.pipe(fileStream);
+				const fileStream = fs.createWriteStream(filePath);
+				resp.pipe(fileStream);
 
-			const fileInfo: FileInfo = {
-				mime,
-				size,
-				path: filePath,
-				filename,
-				ext,
-				buffer: ''
-			};
-			fileStream.on('finish', () => {
-				fileStream.close();
-				fileInfo.buffer = fs.createReadStream(filePath);
-				resolve(fileInfo);
-			});
-			request.on('error', (err) => {
-				logger.error(err);
-				reject(err);
-			});
-			fileStream.on('error', (err) => {
-				logger.error(err);
-				reject(err);
-			});
-			request.end();
+				const fileInfo: FileInfo = {
+					mime,
+					size,
+					path: filePath,
+					filename,
+					ext,
+					buffer: ''
+				};
+				fileStream.on('finish', () => {
+					fileStream.close();
+					fileInfo.buffer = fs.createReadStream(filePath);
+					resolve(fileInfo);
+				});
+				request.on('error', (err) => {
+					reject(err);
+				});
+				fileStream.on('error', (err) => {
+					reject(err);
+				});
+				request.end();
+			} else {
+				reject('Unknown file downloading error');
+			}
 		});
 	});
 };
 
-export const downloadVideo = async (videoUrl: string, saveTo: string, filename: string | number): Promise<FileInfo> => {
+export const downloadVideo = async (videoUrl: string, saveTo: string, filename: string | number): Promise<FileInfo | null> => {
 	let filePath = path.join(saveTo, `${filename}.mp4`);
-	let result;
-	try {
-		result = await youtubeDlExec(videoUrl, {
-			dumpJson: true,
+	let fileInfo: FileInfo = null;
+	const result = await youtubeDlExec(videoUrl, {
+		dumpJson: true,
+		format: '(mp4)[height<=640][height>=360][width<=640][width>=360]'
+	});
+	if (result.duration > 600) {
+		throw `Video is longer than 10 minutes
+			${result.fulltitle ?? ''}
+			${result.webpage_url ?? ''}`;
+	}
+	if (result.extractor === 'youtube') {
+		await downloadYoutubeVideo(result.webpage_url, saveTo, filename);
+	} else {
+		await youtubeDlExec(videoUrl, {
+			output: filePath,
 			format: '(mp4)[height<=640][height>=360][width<=640][width>=360]'
 		});
-		if (result.duration > 600) return null;
-		if (result.extractor === 'youtube') {
-			await downloadYoutubeVideo(result.webpage_url, saveTo, filename);
-		} else {
-			await youtubeDlExec(videoUrl, {
-				output: filePath,
-				format: '(mp4)[height<=640][height>=360][width<=640][width>=360]'
-			});
-			filePath = await convertVideoToMP4(filePath);
-		}
-	} catch (error) {
-		logger.error(error);
-		throw new Error(error);
+		filePath = await convertVideoToMP4(filePath);
 	}
+
 	const size = Math.round(fs.statSync(filePath).size / 1024); // kb
-	if (size > 50000) throw 'File is bigger than 50mb';
+	if (size > 50000) {
+		throw 'Video is bigger than 50MB';
+	}
 	const [name, ext] = path.basename(filePath).split('.');
-	const fileInfo: FileInfo = {
+	fileInfo = {
 		ext,
 		filename: name,
 		mime: '',
@@ -103,7 +105,6 @@ export const downloadYoutubeVideo = (videoUrl: string, saveTo: string, filename:
 		video.pipe(fileStream);
 		video.on('error', (err) => reject(err));
 		video.on('end', () => {
-			logger.info('Video downloaded');
 			resolve();
 		});
 	});
--- packages/vk-api/src/lib/vk-api.ts
+++ packages/vk-api/src/lib/vk-api.ts
@@ -1,30 +1,32 @@
 import { VK as VKAPI, API, getRandomId } from 'vk-io';
 import dotenv from 'dotenv';
+import { logger } from '@yc-bot/shared';
 dotenv.config();
 
 interface IVK {
 	errorChatId: number;
 	api: API;
-	sendError: (error: unknown) => Promise<void>;
+	sendError(error: unknown): Promise<void>;
 }
 
-export default class VK implements IVK {
+class VK implements IVK {
 	errorChatId: number;
 	api: API;
-	constructor(token: string, errorChatId?: number | string) {
-		this.api = new VKAPI({ token: token ?? process.env.VK_TOKEN }).api;
-		this.errorChatId = +errorChatId;
+	constructor() {
+		this.api = new VKAPI({ token: process.env.VK_TOKEN }).api;
+		this.errorChatId = +process.env.VK_ERROR_CHAT_ID;
 	}
 
-	async sendError(error: unknown): Promise<void> {
-		if (this.errorChatId) {
-			let message = '';
-			if (error instanceof Error) {
-				message = error.message;
-			} else {
-				message = JSON.stringify(error);
+	async sendError(error: Error): Promise<void> {
+		if (this.errorChatId && this.api) {
+			try {
+				const message = error.message ?? JSON.stringify(error);
+				await this.api.messages.send({ peer_id: this.errorChatId, random_id: getRandomId(), message });
+			} catch (error) {
+				logger.error(error.message);
 			}
-			await this.api.messages.send({ peer_id: this.errorChatId, random_id: getRandomId(), message });
 		}
 	}
 }
+
+export const vk = new VK();
