c52a5958dd768288230e2763789589f2a7dc2f46
idreamzzer <dreamswave.dev@gmail.com>
2022-05-07 16:47:03 +0300

    added unique post check

--- packages/yandex-api/src/lib/yandex-api.ts
+++ packages/yandex-api/src/lib/yandex-api.ts
@@ -1,5 +1,9 @@
 import { logger } from '@yc-bot/shared';
 import { SQS } from 'aws-sdk';
+import { Session, cloudApi, serviceClients } from '@yandex-cloud/nodejs-sdk';
+import { Event, Context, VKEvent, Post } from '@yc-bot/types';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const ymq = {
 	sendMessage: async (url: string, message: unknown, options?: { apiOptions: object; sendParams: object }): Promise<SQS.SendMessageResult> => {
@@ -23,4 +27,34 @@ export const ymq = {
 			throw error;
 		}
 	}
+};
+
+export const isPostUnique = async (event: Event, context: Context): Promise<boolean> => {
+	const vkEvent: VKEvent = JSON.parse(event.body) ?? {};
+	const post = vkEvent.object as Post;
+	if (!post.id) return false;
+
+	const {
+		serverless: {
+			functions_function_service: { GetFunctionVersionRequest, RemoveFunctionTagRequest, SetFunctionTagRequest }
+		}
+	} = cloudApi;
+	const session = new Session({ iamToken: context.token.access_token ?? process.env.YC_TOKEN });
+	const client = session.client(serviceClients.FunctionServiceClient, 'serverless-functions.api.cloud.yandex.net:443');
+	try {
+		const functionData = await client.getVersion(GetFunctionVersionRequest.fromPartial({ functionVersionId: context.functionVersion }));
+		if (functionData.tags.includes(`postid_${post.id}`)) {
+			console.log('Duplicate');
+			return false;
+		}
+		const prevIdTag = functionData.tags.find((tag) => tag.split('_')[0] === 'postid');
+		if (prevIdTag) {
+			await client.removeTag(RemoveFunctionTagRequest.fromPartial({ functionVersionId: context.functionVersion, tag: prevIdTag }));
+		}
+		await client.setTag(SetFunctionTagRequest.fromPartial({ functionVersionId: context.functionVersion, tag: `postid_${post.id}` }));
+	} catch (error) {
+		logger.error(error);
+		return false;
+	}
+	return true;
 };
